404b103d9ab18e0fb5fd0a5620a55e76de18d20d
OmarAbdeldayem <omar.abdeldayem.personal@outlook.com>
2015-05-31 14:54:26 -0400

    CANBus reader now working.

--- ArduinoCAN/ArduinoCAN.ino
+++ ArduinoCAN/ArduinoCAN.ino
@@ -1,121 +1,148 @@
-/* Welcome to the ECU Reader project. This sketch uses the Canbus library.
-It requires the CAN-bus shield for the Arduino. This shield contains the MCP2515 CAN controller and the MCP2551 CAN-bus driver.
-A connector for an EM406 GPS receiver and an uSDcard holder with 3v level convertor for use in data logging applications.
-The output data can be displayed on a serial LCD.
+/*
+----------------------------------
+UTAT Space Systems CANBus Reader
+----------------------------------
 
-SK Pang Electronics www.skpang.co.uk
+For use with the Sparkfun CANBus shield.
 
-v1.0 28-03-10
+Initial Author: David Harding
+Created: 11/08/2010
+Modified: 5/31/2015
 
 */
 
-//#include <NewSoftSerial.h>
-#include "Canbus.h"
-#include "defaults.h"
-#include "global.h"
-#include "mcp2515.h"
-#include "mcp2515_defs.h"
+#include "SPI.h" // Arduino SPI Library
+#include "MCP2515.h"
 
-//NewSoftSerial sLCD =  NewSoftSerial(3, 14); /* Serial LCD is connected on pin 14 (Analog input 0) */
-#define COMMAND 0xFE
-#define CLEAR   0x01
-#define LINE0   0x80
-#define LINE1   0xC0
+// Pin definitions specific to how the MCP2515 is wired up.
+#define CS_PIN    10
+#define INT_PIN    2
 
-int LED2 = 8;
-int LED3 = 7;
-int interrupt = 2;
+byte i = 0;
+// CAN message frame (actually just the parts that are exposed by the MCP2515 RX/TX buffers)
+Frame message_0, message_1;
+// Create CAN object with pins as defined
+MCP2515 CAN(CS_PIN, INT_PIN);
 
-unsigned char len = 0;
-unsigned char buf[8];
 
-uint8_t stat = 1;
-
-char str[20];
-char data[8];
-
-void setup()
-{
-    
-    pinMode(LED2, OUTPUT);
-    pinMode(LED3, OUTPUT);
-    pinMode(interrupt, INPUT);
-    
+void setup() {
     Serial.begin(9600);
     
-    Serial.println("CANBus Reader");  // For debug use
+    Serial.println("Initializing ...");
     
-    if(Canbus.init(CANSPEED_250))
-    {
-        Serial.println("OK");
+    // Set up SPI Communication
+    // dataMode can be SPI_MODE0 or SPI_MODE3 only for MCP2515
+    SPI.setClockDivider(SPI_CLOCK_DIV2);
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setBitOrder(MSBFIRST);
+    SPI.begin();
+    
+    // Initialise MCP2515 CAN controller at the specified speed and clock frequency
+    int baudRate=CAN.Init(250,16);
+    if(baudRate>0) {
+        Serial.println("MCP2515 Init OK ...");
+        Serial.print("Baud Rate (kbps): ");
+        Serial.println(baudRate,DEC);
+    } else {
+        Serial.println("MCP2515 Init Failed ...");
+    }
+    
+    Serial.println("Ready ...");
+}
+
+void loop() {
+    message_0.id = 0;
+    message_1.id = 0;
+    
+    // This implementation utilizes the MCP2515 INT pin to flag received messages or other events
+    if(CAN.Interrupt()) {
+        // determine which interrupt flags have been set
+        byte interruptFlags = CAN.Read(CANINTF);
+        
+        if(interruptFlags & RX0IF) {
+            // read from RX buffer 0
+            message_0 = CAN.ReadBuffer(RXB0);
+        }
+        if(interruptFlags & RX1IF) {
+            // read from RX buffer 1
+            message_1 = CAN.ReadBuffer(RXB1);
+        }
+        if(interruptFlags & TX0IF) {
+            // TX buffer 0 sent
+        }
+        if(interruptFlags & TX1IF) {
+            // TX buffer 1 sent
+        }
+        if(interruptFlags & TX2IF) {
+            // TX buffer 2 sent
+        }
+        if(interruptFlags & ERRIF) {
+            // error handling code
+        }
+        if(interruptFlags & MERRF) {
+            // error handling code
+            // if TXBnCTRL.TXERR set then transmission error
+            // if message is lost TXBnCTRL.MLOA will be set
+        }
     }
     else
     {
-        Serial.println("ERR");
+        Serial.println("No message received.");
     }
     
-    delay(1000);
+    if(message_0.id>0) {
+        // Print message
+        printCANMessage(message_0, 10);
+    }
     
+    if(message_1.id>0) {
+        // Print message
+        printCANMessage(message_1, 10);
+    }
 }
 
-
-void loop()
-{   
-    //Serial.println(mcp2515_read_status(stat));
-  //  if (digitalRead(interrupt) == 1)
- //   {
- //     Serial.println(digitalRead(interrupt));
-//    }
-    
-/*    if(Canbus.ecu_req(O2_VOLTAGE, data) == 1)
+void printCANMessage(Frame message, unsigned long filter)
+{ 
+    if (filter != 0)
     {
-      Serial.println("Message Recieved");
-    //Serial.println(data[0]);
-    }*/
-    tCAN message;
-    Serial.println(mcp2515_check_message());
-    if (mcp2515_check_message())
-    {
-        digitalWrite(LED2, HIGH);
-        
-        if (mcp2515_get_message(&message))
+        if (message.id == filter)
         {
-            digitalWrite(LED3, HIGH);
             Serial.print("ID: ");
-            Serial.print(message.id,HEX);
-            Serial.print(", ");
-            Serial.print("Data: ");
-            for(int i=0;i<message.header.length;i++)
-            {
+            Serial.println(message.id, HEX);
+            Serial.print("Extended: ");
+            if(message.ide) {
+                Serial.println("Yes");
+            } else {
+                Serial.println("No");
+            }
+            Serial.print("DLC: ");
+            Serial.println(message.dlc,DEC);
+            for(i=0;i<message.dlc;i++) {
                 Serial.print(message.data[i],HEX);
                 Serial.print(" ");
             }
-            Serial.println("");
+            Serial.println();
         }
     }
-    
-    //  tCAN message;
-    //
-    //  if (mcp2515_check_message())
-    //  {
-    //    if (mcp2515_get_message(&message))
-    //    {
-    //      Serial.print("ID: ");
-    //      Serial.print(message.id,HEX);
-    //      Serial.print("\n");
-    //
-    //
-    //      Serial.print("DA: ");
-    //      for(int i=0;i<message.header.length;i++)
-    //      {
-    //        Serial.print(message.data[i],HEX);
-    //        Serial.print(" ");
-    //      }
-    //      Serial.println("");
-    //      }
-    //    }
-    //    else
-    //    {
-    //      Serial.println("No Message Recieved.");
-    //    }
+    else
+    {
+        if (message.id == filter)
+        {
+            Serial.print("ID: ");
+            Serial.println(message.id, HEX);
+            Serial.print("Extended: ");
+            if(message.ide) {
+                Serial.println("Yes");
+            } else {
+                Serial.println("No");
+            }
+            Serial.print("DLC: ");
+            Serial.println(message.dlc,DEC);
+            for(i=0;i<message.dlc;i++) {
+                Serial.print(message.data[i],HEX);
+                Serial.print(" ");
+            }
+            Serial.println();
+        }
+    }
 }
